80946688f7e194fe6991c58c7e6ce9f505a85cef
Lachhab1 <Lachhab1@gmail.com>
2022-02-11 18:37:47 +0100

    fixed dark mode issues

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
+// let dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+// console.log(matched);
 
+// let body = document.querySelector("body");
+// let content = document.getElementById("content");
+
+// if (dark) {
+// 	body.style.backgroundColor = "#1a1a1a";
+// 	content.setAttribute("class", "dark");
+// } else {
+// 	body.style.backgroundColor = "#f5f5f5";
+// 	content.setAttribute("class", "light");
+// }
 function save(number) {
     var element = document.querySelector("input.unit-content");
     number = parseFloat(element.value);
